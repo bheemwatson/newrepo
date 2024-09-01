c0df79d9a1e598613f58503044918751232d0b2c
amol <amol45@gail.com>
2024-09-01 09:11:54 +0000

    This is first commit

---
+++ exampl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#author: amol
+#date: 12/12/2012
+
+set -x
+
+echo "Print disk space"
+df -h
+
+#create a directory
+mkdir gitdir
+
+#change to gitdir
+cd gitdir
+
+#create 2 file
+touch 1.py 2.py
