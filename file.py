f197f2eb1594f4cc3621993f69c16c3ecd7c999d
root <root@ip-172-31-17-162.us-east-2.compute.internal>
2024-09-15 11:07:32 +0000

    edited file.py commit

--- file.py
+++ file.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 print('hello guys')
 a=2
-=======
-<<<<<<< HEAD
 print('hello guy')
->>>>>>> 05f0ef908024eb8435194341a31af2a5e7244688
 if (a==2):
 	print ('correct')
 else:
@@ -14,14 +10,10 @@ else:
 a<=9,
 if (a==1):
 	print('one stand')
-	a++,
+	a =+ 1,
 else:
 	print('its done')
 
 print('its done for today')
 
 print('its interesting for learning')
-=======
-if (a==3)
- then (a*A);
->>>>>>> 60227b1 (Restart commit)
